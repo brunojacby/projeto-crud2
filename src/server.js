806f1456b1f8b7343e79f3306ffa3a064fe57b7f
Bruno Jacby <brunojck@hotmail.com>
2022-12-03 11:23:29 +0000

    adicionado rotas e banco de dados

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const path = require('path')
-
+const db = require('./database')
+const routes = require('./routes')
 const app = express()
+
+//conexao com database
+db.connect()
 
 // definindo o template engine
 app.set('view engine', 'ejs')
@@ -13,12 +17,9 @@ app.use(express.static(path.join(__dirname, 'public')))
 // habilita server para receber dados via post (formulário)
 app.use(express.urlencoded({ extended: true}))
 
-// rotas
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Titulo Teste'
-    })
-})
+// definindo as rotas
+app.use('/', routes)
+
 
 // 404 error (not found)
 app.use((req, res) => {
